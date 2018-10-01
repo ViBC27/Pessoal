6c038c4c5880a283a9da01f3c6379daa3eb0ef05
Vinicius-Monteiro <32230678+Vinicius-Monteiro@users.noreply.github.com>
2018-10-01 20:36:12 -0300

    Só comentários.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function () {//Funcao para expandir e recolher o menu lateral esquerdo após clicar
     $('#nav-btn').on('click touchstart', function (e) {
         $('html').toggleClass('menu-active');
         e.preventDefault();
